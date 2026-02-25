ef538bb2bd450efd5592f1a436495d9622e2d62c
MSubhanAli4210 <subhanali4219@gmail.com>
2026-02-25 19:39:52 +0500

    shifted to render api

--- src/@core/@config/config.ts
+++ src/@core/@config/config.ts
@@ -1,3 +1,3 @@
 export let serverUrl1:string ="https://jsonplaceholder.typicode.com"; 
-// export let serverUrl:string ="https://oms-uqmr.onrender.com/api/v1/";
-export let serverUrl:string ="http://127.0.0.1:8000/api/v1/";
+export let serverUrl:string ="https://oms-uqmr.onrender.com/api/v1/";
+// export let serverUrl:string ="http://127.0.0.1:8000/api/v1/";
